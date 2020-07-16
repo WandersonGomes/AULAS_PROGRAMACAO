092594b813032924633fb16e76441a8d458baf82
WandersonGomes <dersom100@gmail.com>
2020-07-16 18:30:39 -0300

    Codigos Fonte Portugol

---
+++ codigos_aula_02/uri_problem_1003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int variavel_a = 0, variavel_b = 0, soma = 0;
+
+    scanf("%d", &variavel_a);
+    scanf("%d", &variavel_b);
+
+    soma = variavel_a + variavel_b;
+
+    printf("SOMA = %d\n", soma);
+    return 0;
+}
